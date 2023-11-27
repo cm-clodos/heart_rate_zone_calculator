b4d2c6f5da4b0bb6d0f86bda7779104a19b7c1b9
clodos <clodos_m92@hotmail.com>
2023-11-27 17:20:11 +0100

    deployment einstellungen

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: '/heart_rate_zone_calculator/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
